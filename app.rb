4db3fa91777e2eeeab21553424ee7333f0891a5e
Amy Cen <amy.cen@blackrock.com>
2019-06-03 13:25:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 class App < Sinatra::Base
 
+<<<<<<< HEAD
+=======
+  # Define your GET '/' route below and respond with "Hello, World!"
+>>>>>>> 173a39a33ce73d4404106e3d6f74bee3ecb82ef7
   get '/' do
     "Hello, World!"
   end
